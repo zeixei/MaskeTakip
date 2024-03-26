eac9851c991245a4bde0beae4e18ea9d58a844af
Zeynep <Zeynep@DESKTOP-E07QFD0>
2024-03-26 22:35:39 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Abstract;
+using Entities.Concrete;
+
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+
+        private IApplicantService _applicantService; //fieldlar class icinde alt cizgiyle
+
+        public PttManager(IApplicantService applicantService) //constructor new yapildiginda calisir
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        { 
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + "icin maske verildi.");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske VERİLEMEDİ");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+
+            Person person1 = new Person();
+            person1.FirstName = "zeynep sude";
+            person1.LastName = "genç";
+            person1.DateOfBirthYear = 2000;
+            person1.NationalIdentity = 1;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+	}
+}
+
